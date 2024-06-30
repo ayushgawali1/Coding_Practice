627c1010b47fe344c82c9e865c19c6137c16811d
Ayush Gawali <142976942+ayushgawali1@users.noreply.github.com>
2024-06-30 20:00:15 +0530

    Create Multiplication_table.cpp

---
+++ C++/Multiplication_table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i,a;
+    cout<<"Enter the No for Table : ";
+    cin>>a;
+    for(i=1;i<=10;i++){
+        cout<<a<<" x "<<i<<" = "<<a*i<<endl;
+    }
+    return 0;
+}
